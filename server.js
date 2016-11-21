7340a91c5fbe3e576788c4299b235edf21fc01a7
Bertie Scott <b@bertie.io>
2016-11-21 19:07:27 +0000

    worked on setting up servers

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var morgan = require('morgan');
 var sqlite = require('sqlite3');
 var prompt = require('prompt');
 
+var error = {};
+
 var paths = {};
-paths.factorioDir = "/home/bertie/games/factorioServers";
+paths.factorioDir = "/home/bertie/games/factorioServers/";
 paths.workingDir = process.cwd();
 
 var fs = require("fs");
@@ -17,20 +19,8 @@ var pause = false;
 
 var app = express();
 var admin = express.Router();
+var db = new sqlite.Database(file);
 
-app.use(morgan('common'));
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/', admin);
-
-admin.get('/', function(req, res) {
-    adminTemplate = pug.compileFile(__dirname + '/template.pug');
-    html = adminTemplate();
-    res.send(html);
-});
-
-admin.post('/addServer', function(req, res) {
-
-});
 
 function run() {
     app.listen(3000, function() {
@@ -39,17 +29,17 @@ function run() {
 }
 
 function createUser(db, details) {
-    var q = db.prepare("INSERT INTO User (userID, userName, userPassword, userEmail) VALUES (?,?,?,?)");
-		var i;
-		db.get("SELECT * FROM User WHERE userID = (SELECT MAX(userID) FROM User);", function(err, rows) {
-			if(rows === undefined){
-				i = 0;
-			}else{
-				i = rows.userID + 1;
-			}
-		});
+    var i;
+    db.get("SELECT * FROM User WHERE userID = (SELECT MAX(userID) FROM User);", function(err, rows) {
+        if (rows === undefined) {
+            i = 0;
+        } else {
+            i = rows.userID + 1;
+        }
+    });
 
-		q.run([i, details.username, details.password, details.password]);
+    var q = db.prepare("INSERT INTO User (userID, userName, userPassword, userEmail) VALUES (?,?,?,?)");
+    q.run([i, details.username, details.password, details.password]);
     q.finalize();
 
 }
@@ -58,7 +48,7 @@ function createUser(db, details) {
 
 if (!dbExists) {
     pause = true;
-    var db = new sqlite.Database(file);
+    //var db = new sqlite.Database(file);
     console.log("creating Database");
     db.serialize(function() {
         db.run("CREATE TABLE Servers (serverID int, serverDir TEXT);");
@@ -82,11 +72,48 @@ if (!dbExists) {
             console.log('  email: ' + result.email);
             console.log('  password: ' + result.password);
             createUser(db, result);
-            db.close();
             run();
         });
     });
 
 } else {
+    var db = new sqlite.Database(file);
     run();
 }
+
+app.use(morgan('common'));
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/', admin);
+
+admin.get('/', function(req, res) {
+    adminTemplate = pug.compileFile(__dirname + '/template.pug');
+    html = adminTemplate();
+    res.send(html);
+});
+
+admin.get('/addServer', function(req, res) {
+    var i = 0;
+    db.get("SELECT * FROM Servers WHERE serverID = (SELECT MAX(serverID) FROM Servers);", function(err, rows) {
+        if (rows === undefined) {
+            i = 0;
+        } else {
+            i = rows.serverID + 1;
+        }
+        var dir = paths.factorioDir + "server" + i;
+        //	var q = db.prepare("INSERT INTO Server (serverID, ServerDir) VALUES (?,?)");
+        //q.run([i, dir]);
+        //q.finalize();
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+            error.level = "sucess";
+            error.details = "Created Server " + (i + 1) + "in " + dir + "!";
+						res.send(error.level + ":" + error.details);
+        } else {
+            console.log("Directory " + dir + " Exists");
+            error.level = "warn";
+            error.details = "Directory " + dir + " Exists";
+						res.send(error.level + ":" + error.details);
+
+        }
+    });
+});
--- static/script.js
+++ static/script.js
@@ -1,35 +1,34 @@
-(function (window, document) {
-
-    var layout   = document.getElementById('layout'),
-        menu     = document.getElementById('menu'),
-        menuLink = document.getElementById('menuLink');
-
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
-
-        for(; i < length; i++) {
-          if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-          }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
+function confim() {
+    if (confirm("Create New Server?") == true) {
+        startServer();
     }
 
-    menuLink.onclick = function (e) {
-        var active = 'active';
+}
 
-        e.preventDefault();
-        toggleClass(layout, active);
-        toggleClass(menu, active);
-        toggleClass(menuLink, active);
-    };
-
-}(this, this.document));
+function startServer(){
+  console.log("Bang!");
+  infoBox = document.getElementById('sysInfo');
+  var xhr = new XMLHttpRequest();
+  xhr.open("Get", "addServer");
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4){
+      if (xhr.status === 200){
+        var result = xhr.responseText;
+        if(result !== null){
+              console.log(result);
+              var formedResult = result.split(":");
+              if(formedResult[0] == "warn"){
+                  infoBox.innerHTML = formedResult[1];
+                  infoBox.className = "sysWarn";
+                }
+        }
+      }else {
+        console.error(xhr.statusText);
+      }
+    }
+  };
+  xhr.onerror = function (e) {
+  console.error(xhr.statusText);
+};
+  xhr.send(null);
+}
