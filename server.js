21a0230a834eefb0ac2d65dfab5a398e9d2dcae3
Bertie Scott <b@bertie.io>
2016-11-25 15:33:08 +0000

    worked on loading server details to UI

--- server.js
+++ server.js
@@ -7,20 +7,22 @@ var sqlite = require('sqlite3');
 var prompt = require('prompt');
 var wget = require('wget-improved');
 var tar = require('tarball-extract');
+var fs = require("fs");
 
-var opts = {}
+var opts = {};
 opts.port = 3000;
 
 
-var factorio = {}
+var factorio = {};
 factorio.vers = "0.14.20";
-factorio.dl = "https://www.factorio.com/get-download/" + factorio.vers + "/headless/linux64"
-factorio.file = "headless.tar.gz"
+factorio.dl = "https://www.factorio.com/get-download/" + factorio.vers + "/headless/linux64";
+factorio.file = "headless.tar.gz";
 var error = {};
 
 var paths = {};
-paths.factorioDir = "/home/b/games/factorioServers/";
+paths.factorioDir = "/home/bertie/games/factorioServers/";
 paths.workingDir = process.cwd();
+paths.conf = "/factorio/data/server-settings.example.json";
 
 var fs = require("fs");
 var file = paths.workingDir + "/database.db";
@@ -40,15 +42,14 @@ function run() {
 
 function createUser(db, details) {
     var i;
+    var q = db.prepare("INSERT INTO User (userID, userName, userPassword, userEmail) VALUES (?,?,?,?)");
     db.get("SELECT * FROM User WHERE userID = (SELECT MAX(userID) FROM User);", function(err, rows) {
         if (rows === undefined) {
             i = 0;
-            var q = db.prepare("INSERT INTO User (userID, userName, userPassword, userEmail) VALUES (?,?,?,?)");
             q.run([i, details.username, details.password, details.email]);
             q.finalize();
         } else {
             i = rows.userID + 1;
-            var q = db.prepare("INSERT INTO User (userID, userName, userPassword, userEmail) VALUES (?,?,?,?)");
             q.run([i, details.username, details.password, details.email]);
             q.finalize();
         }
@@ -97,7 +98,23 @@ app.use(morgan('common'));
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/', admin);
 
+function listServers() {
+  var list = new array();
+  db.each("SELECT * FROM Servers;", function(err, rows){
+    var confi = fs.readFileSync(paths.factorioDir + "server" + rows.serverID + paths.conf);
+    var conf = JSON.parse(confi);
+    
+    list.push({id: rows.serverID});
+    console.log(list[0]);
+  });
+  if(list === undefined){
+    list = {id: -1};
+  }
+  return list;
+}
+
 admin.get('/', function(req, res) {
+    listServers();
     adminTemplate = pug.compileFile(__dirname + '/template.pug');
     html = adminTemplate();
     res.send(html);
@@ -118,7 +135,7 @@ admin.get('/addServer', function(req, res) {
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir);
             var out = dir + factorio.file;
-            var down = wget.download(factorio.dl, out)
+            var down = wget.download(factorio.dl, out);
             var ee;
             down.on('error', function(err) {
                 console.log(err);
@@ -126,9 +143,9 @@ admin.get('/addServer', function(req, res) {
             down.on('end', function(output) {
               console.log(output);
             tar.extractTarball(out, dir, function(err) {
-                if (err) console.log(err)
-            })
-          })
+                if (err) console.log(err);
+            });
+          });
             error.level = "sucess";
             error.details = "Created Server " + (i) + " in " + dir + "!";
             res.send(error.level + ":" + error.details);
